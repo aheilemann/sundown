2a32139648027043c135cf0c4395e181b215b707
Anders Heilemann <a.heilemann@gmail.com>
2021-05-12 16:42:38 +0200

    Limited Data/Time options, added date-fns

--- pages/order.tsx
+++ pages/order.tsx
@@ -3,6 +3,12 @@ import Router from "next/router";
 import React, { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+import getDay from "date-fns/getDay";
+import { registerLocale } from "react-datepicker";
+import da from "date-fns/locale/da";
+registerLocale("da", da);
 
 import OrderContext from "../contexts/orderContext";
 
@@ -45,7 +51,19 @@ const Order = () => {
   const [bookingDate, setBookingDate] = useState(new Date());
   const [numOfGuests, setNumOfGuests] = useState(1);
   const [email, setEmail] = useState("");
-  console.log(order);
+
+  const isWeekday = (date) => {
+    const day = getDay(date);
+    return day !== 0 && day !== 6;
+  };
+
+  const filterPassedTime = (time) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() < selectedDate.getTime();
+  };
+
   const handleSubmit = async (e) => {
     console.log(e);
     setOrder((order) => ({
@@ -68,11 +86,16 @@ const Order = () => {
               <label>Select date and time:</label>
               <DatePicker
                 selected={bookingDate}
-                onChange={(date) => setBookingDate(date)}
+                locale="da"
+                onChange={(date: Date) => setBookingDate(date)}
                 showTimeSelect
                 dateFormat="Pp"
                 inline
                 required
+                filterDate={isWeekday}
+                filterTime={filterPassedTime}
+                minTime={setHours(setMinutes(new Date(), 0), 16)}
+                maxTime={setHours(setMinutes(new Date(), 0), 23)}
               />
             </Col>
           </Row>
